64e167a32e4bd19dbd241d4f3cf22cd16146cfb8
Tsukasa Watanabe <tswata@gmail.com>
2017-04-15 23:34:43 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
  
-  get 'users/new'
-
-  get 'users/create'
-
   root to: "sessions#new"
+  
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  
   get "signup", to: "users#new"
   resources :users, only:[:new, :create]
   resources :tasks
